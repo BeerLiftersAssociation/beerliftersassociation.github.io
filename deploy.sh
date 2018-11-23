30359dec5bd561203587c811168475f07db94b7b
majway27 <robert.p.may@gmail.com>
2018-11-23 17:52:20 +0000

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ jekyll build --destination ../beerliftersassociation.github.io/
 cd ../beerliftersassociation.github.io/
 git add -A
 git commit -am "Deploy"
+git push
 cd ../marketing-site-source
 echo "Complete"
